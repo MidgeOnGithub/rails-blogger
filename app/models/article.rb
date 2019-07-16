9be3568fcb76a13015631ce069d85169c3f8d78f
Cody <Mason.Cody.M@gmail.com>
2019-07-15 21:48:20 -0400

    Article editing hotfix.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,12 @@ class Article < ApplicationRecord
     has_many :taggings
     has_many :tags, through: :taggings
     
+    def tag_list
+        self.tags.collect do |tag|
+            tag.name
+        end.join(", ")
+    end        
+
     def tag_list=(tag_string)
         tag_names = tag_string.split(",").collect{|s| s.strip.downcase}.uniq
         self.tags = tag_names.collect {|tag| Tag.find_or_create_by(name: tag)}
