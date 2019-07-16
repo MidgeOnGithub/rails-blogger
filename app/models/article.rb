9830387f20c60fc6649f5d65ee14ff928e62a441
Cody <Mason.Cody.M@gmail.com>
2019-07-15 21:43:17 -0400

    Add article tags.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
 class Article < ApplicationRecord
     has_many :comments
+    has_many :taggings
+    has_many :tags, through: :taggings
+    
+    def tag_list=(tag_string)
+        tag_names = tag_string.split(",").collect{|s| s.strip.downcase}.uniq
+        self.tags = tag_names.collect {|tag| Tag.find_or_create_by(name: tag)}
+    end
 end
